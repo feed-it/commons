f8670bf51860334e8036e81913702c3f4c5140ad
Alexis Vasseur <alexis.vasseur@feed-it.io>
2025-09-10 17:01:00 +0200

    fix(table): valid data populate when mismatch columns exists + style

--- src/components/Table/hooks/useTable.ts
+++ src/components/Table/hooks/useTable.ts
@@ -18,46 +18,46 @@ export type SortByType = {
 export default function useTable({ allowMismatch, columns, data, uniqueValueColumn, ref }: useTableProps) {
 	const [localData, setLocalData] = useState(data);
 	const [sortBy, setSortBy] = useState<SortByType | null>(null);
-
+	
 	useEffect(() => {
 		setLocalData(data);
 	}, [data]);
-
+	
 	const sortedData = useMemo(() => {
 		if (!sortBy) {
 			return localData;
 		}
-
+		
 		const collator = Intl.Collator('fr-FR', {
 			ignorePunctuation: true,
 			numeric: true,
 			sensitivity: 'base',
 		});
-
+		
 		if (sortBy.order === 'desc') {
 			return localData.toSorted((a, b) => collator.compare(b[sortBy.prop], a[sortBy.prop]));
 		}
-
+		
 		return localData.toSorted((a, b) => collator.compare(a[sortBy.prop], b[sortBy.prop]));
 	}, [localData, sortBy]);
-
+	
 	const updateData = useCallback(
 		(id: any, prop: string, value: any) => {
 			setLocalData((prev) => {
 				const index = prev.findIndex((x) => x[uniqueValueColumn] === id);
 				if (index === -1) return prev;
-
+				
 				const newRow = {
 					...prev[index],
 					[prop]: value,
 				};
-
+				
 				return prev.toSpliced(index, 1, newRow);
 			});
 		},
-		[uniqueValueColumn]
+		[uniqueValueColumn],
 	);
-
+	
 	const deleteRow = useCallback(
 		(id: any) => {
 			setLocalData((prev) => {
@@ -66,28 +66,28 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 				return prev.toSpliced(index, 1);
 			});
 		},
-		[uniqueValueColumn]
+		[uniqueValueColumn],
 	);
-
+	
 	const updateColumn = useCallback((oldColumn: string, newColumn: string) => {
 		setLocalData((prev) => {
 			const result = [];
-
+			
 			for (const row of prev) {
 				const newRow = {
 					...row,
 					[newColumn]: row[oldColumn],
 				};
-
+				
 				delete newRow[oldColumn];
-
+				
 				result.push(newRow);
 			}
-
+			
 			return result;
 		});
 	}, []);
-
+	
 	const deleteColumn = useCallback((prop: string) => {
 		setLocalData((prev) => {
 			return prev.map((row) => {
@@ -96,42 +96,42 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 			});
 		});
 	}, []);
-
+	
 	const reviewedColumns: ExtendedColumn[] = useMemo(() => {
 		if (!allowMismatch) return columns;
-
+		
 		const dataColumns = new Set<string>();
-
+		
 		//* Get columns from data
 		for (const row of localData) {
 			for (const column of Object.keys(row)) {
 				if (column === 'warnings' || dataColumns.has(column)) continue;
 				if (column === uniqueValueColumn && columns.findIndex((x) => x.prop === uniqueValueColumn) === -1)
 					continue;
-
+				
 				dataColumns.add(column);
 			}
 		}
-
+		
 		const result: ExtendedColumn[] = [...columns];
 		const remainingColumns = [];
-
+		
 		//* Detect all correct columns
 		for (const column of columns) {
 			if (dataColumns.has(column.prop)) {
 				dataColumns.delete(column.prop);
-
+				
 				const values = localData.map((x) => x[column.prop]);
 				if (values.every((x) => !x)) {
 					remainingColumns.push(column);
 				}
-
+				
 				continue;
 			}
-
+			
 			remainingColumns.push(column);
 		}
-
+		
 		//* For every remaining columns (from data) which are not exactly correct with the models (columns variable)
 		//* Try to use the misspell function
 		const model = remainingColumns.reduce((acc, cur) => {
@@ -142,13 +142,13 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 			acc[cur] = cur;
 			return acc;
 		}, {} as { [key: string]: any });
-
+		
 		const { correction } = misspelled.check([misspellInput], model);
-
+		
 		//* Add all corrected columns
 		for (const [newColumn, oldColumn] of Object.entries(correction)) {
 			const colIndex = result.findIndex((x) => x.prop === newColumn);
-
+			
 			const obj = {
 				...result[colIndex],
 				error: {
@@ -157,19 +157,19 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 					columns: remainingColumns,
 				},
 			};
-
+			
 			result.splice(colIndex, 1, obj);
-
+			
 			//* Remove oldColumn e.g: typeSiet from dataColumns to prevent it to be showed twice
 			dataColumns.delete(oldColumn);
-
+			
 			//* Also remove the matchedColumn (newColumn var) from remainingColumns
 			const index = remainingColumns.findIndex((x) => x.prop === newColumn);
 			if (index > -1) {
 				remainingColumns.splice(index, 1);
 			}
 		}
-
+		
 		for (const column of [...dataColumns]) {
 			result.push({
 				label: column,
@@ -180,42 +180,41 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 				error: { type: 'not-found', columns: remainingColumns },
 			});
 		}
-
+		
 		return result;
 	}, [allowMismatch, columns, localData, uniqueValueColumn]);
-
+	
 	// --------------------------------------------------- Custom ref ------------------------------------------------------
 	const getData = useCallback(() => {
 		const validData = [];
-		if (!reviewedColumns.some((x) => Object.hasOwn(x, 'error'))) {
+		if (!reviewedColumns.some((x) => Object.hasOwn(x, 'error') && x.error?.type !== 'mismatch')) {
 			for (const row of localData) {
 				if (!Object.hasOwn(row, 'warnings')) {
 					validData.push(row);
-					continue;
 				}
 			}
 		}
-
+		
 		const errorOnNull = new Map();
 		const errorOnUnique = new Map();
 		const errorOnOthers = new Map();
-
+		
 		for (const column of reviewedColumns) {
 			const prop = column.error?.type === 'mismatch' && column.error.target ? column.error.target : column.prop;
-
+			
 			for (const row of localData) {
 				if (!(column.allowNull ?? true) && !row[prop] && !errorOnNull.has(row[uniqueValueColumn])) {
 					errorOnNull.set(row[uniqueValueColumn], row);
 				}
-
+				
 				if (column.unique) {
 					const matches = localData.filter((x) => x[prop] === row[prop]).length;
-
+					
 					if (matches > 1 && !errorOnUnique.has(row[uniqueValueColumn])) {
 						errorOnUnique.set(row[uniqueValueColumn], row);
 					}
 				}
-
+				
 				if (typeof column.validate === 'function') {
 					if (column.validate(row[prop]) && !errorOnOthers.has(row[uniqueValueColumn])) {
 						errorOnOthers.set(row[uniqueValueColumn], row);
@@ -223,7 +222,7 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 				}
 			}
 		}
-
+		
 		return {
 			data: localData,
 			validData,
@@ -232,9 +231,9 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 			errorOnOthers: [...errorOnOthers.values()],
 		};
 	}, [localData, reviewedColumns, uniqueValueColumn]);
-
+	
 	const getColumns = useCallback(() => reviewedColumns, [reviewedColumns]);
-
+	
 	useImperativeHandle(
 		ref,
 		() => {
@@ -243,9 +242,9 @@ export default function useTable({ allowMismatch, columns, data, uniqueValueColu
 				getData,
 			};
 		},
-		[getColumns, getData]
+		[getColumns, getData],
 	);
-
+	
 	return {
 		sortedData,
 		sortBy,
