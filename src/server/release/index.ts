63403be1813f6dcb94b4e7b3275b4ec99ff7bad0
Alexis Vasseur <alexis.vasseur@feed-it.io>
2025-11-04 12:15:45 +0100

    style(release): use chalk to color terminal outputs

--- src/server/release/index.ts
+++ src/server/release/index.ts
@@ -4,6 +4,7 @@ import { exit } from 'process';
 import yaml from 'js-yaml';
 import { checkbox, select } from '@inquirer/prompts';
 import * as path from 'path';
+import chalk from 'chalk';
 
 export type ReleaseScriptParams = {
 	/**
@@ -47,6 +48,32 @@ export class ReleaseScript {
 		if (params.haveBeta) this.haveBeta = params.haveBeta;
 	}
 
+	/**
+	 * Test parameters provided in constructor call
+	 */
+	private beforeStart() {
+		console.info(chalk.blue.bold('1. Testing given parameters before releasing app'));
+
+		// Test serverName
+		console.info(chalk.blue('1.a. Testing ssh connexion...'));
+		try {
+			exec(`ssh -T ${this.serverName}`);
+		} catch (error) {
+			console.error(chalk.red(`Failed to connect to ${this.serverName}`), error);
+			exit(1);
+		}
+
+		// Test YAML file
+		console.info(chalk.blue('1.b. Check if YAML file exists...'));
+		const filePath = `${process.cwd()}/release/${this.yamlFilename}.yaml`;
+		if (!existsSync(filePath)) {
+			console.error(chalk.red(`Unable to find YAML file at: ${filePath}`));
+			exit(1);
+		}
+
+		console.info(chalk.green('All tests passed!\n'));
+	}
+
 	async start() {
 		this.beforeStart();
 
@@ -86,46 +113,24 @@ export class ReleaseScript {
 			exit(1);
 		}
 
-		console.info(`\u001b[32m${this.appName} successfully updated !\u001b[0m`);
+		console.info(chalk.green.bold(`${this.appName} successfully updated !`));
 		console.info('Make sure to update main/develop now !');
-	}
-
-	/**
-	 * Test parameters provided in constructor call
-	 */
-	private beforeStart() {
-		console.info('\u001b[34mTesting given parameters before releasing app\u001b[0m');
-
-		// Test serverName
-		console.info('\u001b[34m1.Testing ssh connexion...\u001b[0m');
-		try {
-			exec(`ssh -T ${this.serverName}`);
-		} catch (error) {
-			console.error(`\u001b[31mFailed to connect to ${this.serverName}\u001b[0m\n`, error);
-			exit(1);
-		}
-
-		// Test YAML file
-		console.info('\u001b[34m2.Check if YAML file exists...\u001b[0m');
-		const filePath = `${process.cwd()}/release/${this.yamlFilename}.yaml`;
-		if (!existsSync(filePath)) {
-			console.error(`\u001b[31mUnable to find YAML file at: ${filePath}\u001b[0m`);
-			exit(1);
-		}
-
-		console.info('\u001b[32mAll tests passed!\u001b[0m');
 	}
 
 	/**
 	 * Make a release, either for the prod or the beta
 	 */
 	private release(release: 'prod' | 'beta', typeVersion?: string) {
+		console.info(chalk.blue.bold(`\nReleasing ${release} ${typeVersion ? `on ${typeVersion}` : ''}\n`));
+
 		// Update the package version if the release is for production
 		if (release === 'prod') {
-			console.info('\u001b[34m1. Update package version\u001b[0m');
+			console.info(chalk.blue('1. Update package version'));
 			exec(`npm version --no-git-tag-version ${typeVersion}`, {
 				stdio: 'inherit',
 			});
+		} else {
+			console.info(chalk.blue('1. Skipping updating package version'));
 		}
 
 		const digest = this.docker(release);
@@ -138,19 +143,22 @@ export class ReleaseScript {
 	 * Build the image, push it to the Docker Hub repository and return the new image digest.
 	 */
 	private docker(release: 'prod' | 'beta') {
+		console.info(chalk.blue.bold('\n2. Build Docker image and send it to Docker Hub repository'));
+
 		//* 1. Build Docker image.
-		console.info('\u001b[34m3.a Build image...\u001b[0m');
+		console.info(chalk.blue('2.a Build image'));
 		exec(`docker build -t ${this.dockerImage}:${release} .`, {
 			stdio: 'inherit',
 		});
 
 		//* 2. Push Docker image to the Docker Hub repository.
-		console.info('\u001b[34m3.b Push image...\u001b[0m');
+		console.info(chalk.blue('2.b Push image'));
 		exec(`docker push ${this.dockerImage}:${release}`, {
 			stdio: 'inherit',
 		});
 
 		//* 3. Fetch the new Docker image digest.
+		console.info(chalk.blue('2.c Fetch docker image digest'));
 		const digest = exec(`docker inspect --format="{{index .RepoDigests}}" ${this.dockerImage}:${release}`, {
 			encoding: 'utf-8',
 		})
@@ -160,9 +168,11 @@ export class ReleaseScript {
 			.replace(`${this.dockerImage}@`, '');
 
 		if (!digest) {
-			throw new Error('Any Docker image digest found.');
+			console.error(chalk.red('Any Docker image digest found.'));
+			exit(1);
 		}
 
+		console.info(chalk.green('Docker image successfully built and sent to Docker Hub repository'));
 		return digest;
 	}
 
@@ -170,6 +180,8 @@ export class ReleaseScript {
 	 * Update the Kubernetes YAML config file with the new Docker image digest.
 	 */
 	private yaml(digest: string, release: 'prod' | 'beta') {
+		console.info(chalk.blue.bold('\n3. Updating Kubernetes YAML config file'));
+
 		const yamlContent = readFileSync(`${process.cwd()}/release/${this.yamlFilename}.yaml`, 'utf-8');
 		const data: any[] = yaml.loadAll(yamlContent);
 
@@ -180,8 +192,8 @@ export class ReleaseScript {
 				const oldImage = data[i].spec.template.spec.containers[0].image;
 				const newImage = `${this.dockerImage}:${release}@${digest}`;
 
-				console.info('\x1b[31m%s\x1b[0m', 'Old Docker image:', oldImage);
-				console.info('\x1b[32m%s\x1b[0m', 'New Docker image:', newImage);
+				console.info(chalk.red('Old Docker image: '), oldImage);
+				console.info(chalk.green('New Docker image: '), newImage);
 
 				newYamlContent = newYamlContent.replace(oldImage, newImage);
 				break;
@@ -189,19 +201,23 @@ export class ReleaseScript {
 		}
 
 		writeFileSync(`${process.cwd()}/release/${this.yamlFilename}.yaml`, newYamlContent, 'utf-8');
+
+		console.info(chalk.green('Kubernetes YAML config file updated'));
 	}
 
 	/**
 	 * Send the updated Kubernetes YAML config file to the server.
 	 */
 	private scp() {
-		console.info('\u001b[34m4. Update server kubernetes file...\u001b[0m');
+		console.info(chalk.blue.bold('\n4. Send Kubernetes YAML config file to the server'));
+
 		const filePath = `${process.cwd()}/release/${this.yamlFilename}.yaml`;
 
 		exec(`scp -P 91 ${filePath} ${this.serverName}:.kube-yaml/`, {
 			stdio: 'inherit',
 		});
-		console.info('\x1b[36m%s\x1b[0m', `Kubernetes YAML Config sent to ${this.serverName}`);
+
+		console.info(chalk.green(`Kubernetes YAML Config sent to ${this.serverName}`));
 	}
 
 	/**
@@ -210,7 +226,7 @@ export class ReleaseScript {
 	private git(release: 'prod' | 'beta') {
 		const { version: packageVersion } = JSON.parse(readFileSync(`${process.cwd()}/package.json`, 'utf-8'));
 
-		console.info(`\u001b[34m2. Commit package${release === 'prod' ? ' and create git tag' : ''}\u001b[0m`);
+		console.info(chalk.blue.bold(`5. Commit package${release === 'prod' ? ' and create git tag' : ''}`));
 
 		// Push new package.json version
 		let commitMessage = `chore(release): ${packageVersion}`;
@@ -228,5 +244,7 @@ export class ReleaseScript {
 				stdio: 'inherit',
 			});
 		}
+
+		console.info(chalk.green(`Commit pushed ${release === 'prod' ? 'and git tag created' : ''}`));
 	}
 }
